f5c9f04af031fd9f7d12078e2e1d877b741ee0c8
Quentin Lambert <Quentin.Lambert@solidaris.be>
2021-07-01 12:14:49 +0200

    dev: express.static port 3000

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,9 +3,12 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use((req, res, next) => {
+  console.log('req.url', req.url);
+  next();
 });
+
+app.use(express.static('.'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
