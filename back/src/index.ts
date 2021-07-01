34a13ab5b1f90cadaa0888c8fecc882acd32d761
Quentin Lambert <Quentin.Lambert@solidaris.be>
2021-07-01 14:36:45 +0200

    dev: added public dir and test public html

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,14 +3,15 @@ import serveIndex from 'serve-index';
 
 const app = express();
 const port = 3000;
+const publicDir = './public';
 
 app.use((req, res, next) => {
   console.log('req.url', req.url);
   next();
 });
 
-app.use(express.static('.'));
-app.use(serveIndex('.', {icons: true}));
+app.use(express.static(publicDir));
+app.use(serveIndex(publicDir, {icons: true}));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
