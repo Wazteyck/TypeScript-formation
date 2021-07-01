ed10e60f1be28e43819331e04a9d7ef08301931b
Quentin Lambert <Quentin.Lambert@solidaris.be>
2021-07-01 15:59:15 +0200

    dev: webpack works

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,1 +1,4 @@
 console.log("Start the ts");
+
+const toto: number = 45;
+console.log("toto: ", toto);
---
+++ front/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
