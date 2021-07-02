20732a678b4382c531d337b5e346cfe76243ade7
Quentin Lambert <Quentin.Lambert@solidaris.be>
2021-07-02 11:40:52 +0200

    refactor: no any in typescript

--- front/src/Mendeleev.ts
+++ front/src/Mendeleev.ts
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import { ChemicalElt } from "./interfaces/ChemicalElt";
 
 import "./style.scss";
 
@@ -8,8 +9,12 @@ export class Mendeleev {
   }
 
   async init() {
-    const csv = await d3.csv("./atomes.csv");
+    const csv: ChemicalElt[] = (await d3.csv(
+      "./atomes.csv"
+    )) as unknown as ChemicalElt[];
     console.log("csv: ", csv);
+
+    const data = csv.filter((d) => d.Group && d.Period);
 
     // plug csv into html
     const div = document.querySelector("div.tableau") as Element;
@@ -20,15 +25,15 @@ export class Mendeleev {
     }*/
     console.log("div: ", div);
 
-    const selection = d3.select(div).selectAll("div").data(csv);
+    const selection = d3.select(div).selectAll("div").data(data);
     selection
       .enter()
       .append("div")
       .style("transform", (d) => {
-        const x = (d as any).Group * 3;
-        const y = (d as any).Period * 3;
+        const x = d.Group * 3;
+        const y = d.Period * 3;
         return `translate(${x}em, ${y}em)`;
       })
-      .text((d) => (d as any).Symbol);
+      .text((d) => d.Symbol + "\n" + d.NumberofNeutrons);
   }
 }
---
+++ front/src/interfaces/ChemicalElt.ts
@@ -0,0 +1,6 @@
+export interface ChemicalElt {
+  Symbol: string;
+  Period: number;
+  Group: number;
+  NumberofNeutrons: number;
+}
